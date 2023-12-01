92c12f7bdec356012e2f4c215c988a45de5ad76e
Stephen Grier <git@grier.org.uk>
2023-12-01 21:06:37 +0000

    Add code for day 1 part 2
    Add replace_number_words_with_digits function to convert number words to
    digits. In the input data some of the words overlap eg "eightwo", so
    keep the first and last letters of each number word when doing the
    replacement to avoid losing the second number in such situations.

--- 2023/1/part1.py
+++ 2023/1/part1.py
@@ -34,17 +34,32 @@ def get_last_digit(str):
     return None
 
 
+def replace_number_words_with_digits(str):
+    str = str.replace('one', 'o1e')
+    str = str.replace('two', 't2o')
+    str = str.replace('three', 't3e')
+    str = str.replace('four', 'f4r')
+    str = str.replace('five', 'f5e')
+    str = str.replace('six', 's6x')
+    str = str.replace('seven', 's7n')
+    str = str.replace('eight', 'e8t')
+    str = str.replace('nine', 'n9e')
+    return str
+
+
 def main():
     input_file = 'input'
     sum_of_all = 0
+    sum_of_all_2 = 0
     lines = read_input_file(input_file)
     print(len(lines), 'lines in input file')
 
     for line in lines:
-        print(line,": ",get_calibration_value_from_line(line))
         sum_of_all += get_calibration_value_from_line(line)
+        sum_of_all_2 += get_calibration_value_from_line(replace_number_words_with_digits(line))
 
     print(f"Sum of all calibration values is: {sum_of_all}")
+    print(f"Sum of all calibration values (for part 2) is: {sum_of_all_2}")
 
 
 if __name__ == '__main__':
